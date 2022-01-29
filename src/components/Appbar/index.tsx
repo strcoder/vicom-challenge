5450fe42ed84d66df0a021ef68f5a1bf8f51a873
Antonio Guzman <aguzmanv09@gmail.com>
2022-01-28 18:46:08 -0600

    Adding styles to appbar

--- src/components/Appbar/index.tsx
+++ src/components/Appbar/index.tsx
@@ -1,32 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './styles.scss';
 
 const Appbar = () => {
   return (
-    <header>
-      <section className=''>
+    <header className='Header'>
+      <section className='Header__ad'>
         <p>Envio gratis en pedidos desde $699</p>
       </section>
-      <section>
-        <figure>
+      <section className='Header__container'>
+        <figure className='Header__container--logo'>
           <img src='/assets/logo.png' alt='La visconia' />
         </figure>
-        <nav>
-          <ul>
-            <li>Cocción</li>
-            <li>Preparación</li>
-            <li>Mesa y accesorios</li>
-            <li>Consumibles</li>
-            <li>Electrodomesticos</li>
-            <li>Línea profesional</li>
-          </ul>
+        <nav className='Header__container--nav'>
+          <Link to='/'>Cocción</Link>
+          <Link to='/'>Preparación</Link>
+          <Link to='/'>Mesa y accesorios</Link>
+          <Link to='/'>Consumibles</Link>
+          <Link to='/'>Electrodomesticos</Link>
+          <Link to='/'>Línea profesional</Link>
         </nav>
-        <div>
-          <button type='button' title='Abrir menu de usuario'>
+        <div className='Header__container--actions'>
+          <button
+            type='button'
+            className='btn-link'
+            title='Abrir menu de usuario'
+          >
             <figure>
               <img src='/assets/user.png' alt='Menu de usuario' />
             </figure>
           </button>
-          <button type='button'>
+          <button
+            type='button'
+            className='btn-link'
+            title='Ver carrito de compras'
+          >
             <figure>
               <img src='/assets/cart.png' alt='Carrito de compras' />
             </figure>
